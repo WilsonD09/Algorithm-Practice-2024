05bd75122f7d43ad324bb7a535e0376055ca266d
WilsonD09 <wilsond34@nycstudents.net>
2024-10-25 12:27:52 -0400

    h

--- index.js
+++ index.js
@@ -21,16 +21,17 @@ console.log(parkingSpaces(7, "C..C..C", "CCC.CC."));
   }
   console.log(binary);
 }
- */
+ let decimalNumber = 42;
+let binaryNumber = decimalNumber.toString(2);
+console.log(binaryNumber);*/
 
-decimalBinary(17);
-function decimalBinary(decimal) {
+/* function decimalBinary(decimal) {
   let binary = "";
   i = 8;
   while (decimal > 0) {
     i--;
-    if ((2 ^ i) < decimal) {
-      decimal -= 2 ^ i;
+    if (Math.pow(2, i) < decimal) {
+      decimal = decimal - Math.pow(2, i);
       binary.concat("1");
     } else {
       binary.concat("0");
@@ -40,3 +41,16 @@ function decimalBinary(decimal) {
 }
 
 console.log(decimalBinary(17));
+
+function decimaltoBinary(decimal) {
+  let binary = "";
+  while (decimal > 1) {
+    let remainder = decimal % 2;
+    binary = remainder.toString() + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
+}
+
+console.log(decimaltoBinary(176)); */
+console.log("hello world");
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,28 @@
-  console.log(today);
+function decimalBinary(decimal) {
+  let binary = "";
+  i = 8;
+  while (decimal > 0) {
+    i--;
+    if (Math.pow(2, i) < decimal) {
+      decimal = decimal - Math.pow(2, i);
+      binary.concat("1");
+    } else {
+      binary.concat("0");
+    }
+  }
+  return binary;
+}
+
+console.log(decimalBinary(17));
+
+function decimaltoBinary(decimal) {
+  let binary = "";
+  while (decimal > 1) {
+    let remainder = decimal % 2;
+    binary = remainder.toString() + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
+}
+
+console.log(decimaltoBinary(176));
